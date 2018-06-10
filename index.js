bf85de0052163f94d3ee22314f3091bc3552d378
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 18:51:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
-  const message = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  const message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   cart = []
   return message
 }
