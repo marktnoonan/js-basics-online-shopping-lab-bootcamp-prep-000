bf1c3ef199f50a6f400e8f09d330d0289d4657a7
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 18:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push({[item]: randomPrice()})
+ cart.push({itemName: item, itemPrice: randomPrice()})
 }
 
 function viewCart() {
